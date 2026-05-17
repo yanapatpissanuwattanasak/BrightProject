df228c96ba91e7184e8d0451f7cfc1b641d18716
yanapat <ynp.bright@gmail.com>
2026-05-18 02:15:07 +0700

    fix ngx

--- src/lib/rpsSocket.ts
+++ src/lib/rpsSocket.ts
@@ -4,7 +4,8 @@ export const RPS_WS_URL = import.meta.env.VITE_RPS_WS_URL as string | undefined
 
 export function createRpsSocket(): Socket | null {
   if (!RPS_WS_URL) return null
-  return io(`${RPS_WS_URL}/rps`, {
+  return io(`${RPS_WS_URL}/socket.io`, {
+    path: '/rps',
     transports: ['websocket'],
     autoConnect: false,
     reconnectionAttempts: 3,
